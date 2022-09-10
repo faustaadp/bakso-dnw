41be8570700698f24622d8c67a3a4b4a7ea3fd7e
R. Fausta Anugrah Dianparama <t-fausta.dianparama@traveloka.com>
2022-09-10 13:03:24 +0700

    kasir

---
+++ kasir/forms.py
@@ -0,0 +1,21 @@
+# from django import forms
+# from .models import Menu, Kategori
+
+# class FormMenu(forms.ModelForm):
+#     class Meta:
+#         model = Kegiatan
+#         fields = ['nama', 'harga']
+#         widgets = {
+#             'nama' : forms.TextInput(attrs={'class': 'form-control'}),
+#             'deskripsi' : forms.TextInput(attrs={'class': 'form-control'}),
+#         }
+
+# class FormKategori(forms.ModelForm):
+#     class Meta:
+#         model = Kategori
+#         fields = ['nama']
+#         widgets = {
+#             'nama' : forms.TextInput(attrs={'class': 'form-control'}),
+#         }
+
+
