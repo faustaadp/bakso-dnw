504e3369cc10c9ab197168f13ef3189e13388c1a
faustaadp <32033536+faustaadp@users.noreply.github.com>
2023-01-13 16:31:58 +0700

    Fausta/backend transaksi (#4)
    * Membuat backend transaksi sederhana
    
    * Membuat backend transaksi sederhana 2

--- kasir/admin.py
+++ kasir/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Kategori, Menu
+from .models import Kategori, Menu, Transaksi, DetailTransaksi
 
 # Register your models here.
 class KategoriAdmin(admin.ModelAdmin):
@@ -7,5 +7,13 @@ class KategoriAdmin(admin.ModelAdmin):
 admin.site.register(Kategori, KategoriAdmin)
 
 class MenuAdmin(admin.ModelAdmin):
-    list_display    = ['nama']
+    list_display    = ['nama', 'harga', 'kategori', 'gambar']
 admin.site.register(Menu, MenuAdmin)
+
+class TransaksiAdmin(admin.ModelAdmin):
+    list_display    = ['waktu', 'total_harga']
+admin.site.register(Transaksi, TransaksiAdmin)
+
+class DetailTransaksiAdmin(admin.ModelAdmin):
+    list_display    = ['menu', 'jumlah', 'transaksi']
+admin.site.register(DetailTransaksi, DetailTransaksiAdmin)
